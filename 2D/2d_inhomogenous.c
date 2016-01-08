5080946372c87fec797cfc836e35ffc19dc82bb4
Paul Ionele <p.ionele@gmail.com>
2016-01-07 21:45:12 -0500

    Some progress.

--- 2D/2d_inhomogenous.c
+++ 2D/2d_inhomogenous.c
@@ -2,83 +2,87 @@
 Diffusion simulation on 2D, inhomogenous lattice. 
 
 Representation of lattice homogeneity.
-10 10 10 10
-00 00 00 00
+101010
+000000
 
 */
 
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
-#include "string.h"
+#include "string.h" //IS THIS LIB REQUIRED???
 
-// Characterizations of entire inhomogenous lattice.
-#define N 500000	//number of particles
-#define xC 10		//x length of cellular space
-#define yC 10		//y length of cellular space
-#define xE 10		//x length of extracellular space
-#define yE 10		//y length of extracellular space
-#define nU 21		//number of unit cells in row
-#define nR 21		//number of rows
+// Parameters for characterization of entire 2D inhomogenous lattice.
+#define N 100000	//number of particles
+#define xC 11		//x length of cellular space
+#define yC 11		//y length of cellular space
+#define xE 11		//x length of extracellular space
+#define yE 11		//y length of extracellular space
+#define nU 3		//number of unit cells in row
+#define mU 1        //number of unit cells in column (NOT USED)
+#define mR 2		//number of rows
+#define nR 0        //number of columns (NOT USED)
 
 int main(){
-	//Index variables for the for-loops and time-step limit.
-	int i;
-	int t, tmax = 5000;
+	//Index variables; for-loops and time-step limit.
+	int i, j;
+	int t, tmax = 1000;
 
-	//x unit cell length (xU), total array length (xL) and centered start (SP).
+	//Unit cell dimensions.
 	int xU = xC + xE;
+	int yU = yC + yE;
+	//Total lattice/array dimensions.
 	int xL = xU*nU;
-	int SP = (int)(xL/2.0)+5;
+	int yL = yU*mR;
+	
+	//Start lattice site (x,y); currently a cellular region.
+	int xSP = (int)(xL/2.0) - (int)(xC/2.0) - 1;
+	int ySP = (int)(yL/2.0) - (int)(yC/2.0) - 1;
 
-	///NEW VARIABLES DEFINED....
-	int modx; //Modified x-pos. of ith particle. Returns: [0,xU-1]
-	float testx = 0; //Possible future position of particle. Returns: [0,xL-1]
-	float modtestx = 0;
+	//'Modified' x,y-pos. of ith particle. Returns [0,xU-1 or yU-1]
+	int modx;
+	int mody;
+	//Possible future position of particle. Returns: [0,xL-1 or yL-1]
+	double testx = 0;
+	double texty = 0;
+	//'Modified' possible future position of particle.
+	double modtestx = 0;
+	double modtexty = 0;
+	//State cooresponding to particle position; (0: extracellular, 1: cellular).
 	int state = 0;
 	int teststate = 0;
 
-	//Stepsize and probabilities for intracellular (0) and extracellular diffusion (1).
-	//Probablities crossing from intra to extra (pie) or extra to intra (pei). 
-	float a = 1.0;
-	float pli = 0.2, pri = 0.2, psi = 1.0-pli-pri;
-	float ple = 0.3, pre = 0.3, pse = 1.0-ple-pre;
-
-	//For passive transport must be coupled. Active transport, no.
-	float pie = 0.05;
-	float pei = (0.2/0.3)*pie; 
-
-	//Random variable and variables for MSD calculations.
-	float rnd;
-	float sum_x, sum_x2, avg_x, avg_x2;
-
-	//Shifted index and an array to hold particle positions.
-	int sxi; //is this still sxi? Does that name make sense?
+	int sxi; //used in converting ith particle position to integer.
 	int ucop; //unit cell of particle.
 	int ucot; //unit cell of test.
-	float x[N] = {[0 ... (N-1)] = SP}; //designated initializer to set start positions
+	double x[N] = {[0 ... (N-1)] = SP}; //designated initializer to set start positions
 	//for(i = 0; i < N; i++){x[i] = SP;} //initialize with a rolling loop instead.
 
+	//Particle density distribution array.
+	int rho[xL];
+
+	//Stepsize and stepping probablities (arb. chosen); equal for unbiased motion.
+	double a = 1.0;
+	double pli = 0.2, pri = 0.2, psi = 1.0-pli-pri; //Intracellular less diffusive.
+	double ple = 0.3, pre = 0.3, pse = 1.0-ple-pre; //Extracellular more diffusive.
+	//Coupled probablities crossing from intra to extra (pie) or extra to intra (pei). 
+	double pie = 0.05; //Arbitrarily chosen.
+	double pei = (pli/ple)*pie; 
+
+	//Random variable and variables for MSD calculations.
+	double rnd;
+	double sum_x, sum_x2, avg_x, avg_x2;
 
 	//File naming and preparation. Why do we get a segmentation fault below?
 	//char *path = "/home/paul/Documents/thesis/particle-diffusion/data/";
 	//char *f1 = strcat(path,"TEST1.txt");
 	//char *f2 = strcat(path,"TEST1-stats.txt");
-	char *f1 = "/home/paul/Documents/thesis/particle-diffusion/data/TEST1.txt";
-	char *f2 = "/home/paul/Documents/thesis/particle-diffusion/data/TEST1-stats.txt";
+	char *f1 = "/home/paul/Documents/thesis/particle-diffusion/data/t000.txt";
+	char *f2 = "/home/paul/Documents/thesis/particle-diffusion/data/t000_stats.txt";
 	FILE *outdists, *outstats;
 	outdists = fopen(f1, "w");
 	outstats = fopen(f2, "w");
 	
-	//Need to have rho pre-defined for use in the main loop.
-	int *rho;
-	rho = malloc(xL*sizeof(int));
-	
-	//No need to touch: create density array and print out first distribution.
-	for(i = 0; i < N; i++){sxi = (int)x[i]; rho[sxi]++;}
-	for(i = 0; i < xL; i++){fprintf(outdists, "%d ", rho[i]);}
-	fprintf(outdists, "\n");
-
 	//--------------------------------------------------------------------------
 	//Below is where the main work is done.
 	for (t = 0; t < tmax; t++){
@@ -108,7 +112,7 @@ int main(){
 
 			if(state == 1){
 				//Apply intracellular conditions to test particle.
-				rnd = (float)rand()/(float)RAND_MAX;
+				rnd = (double)rand()/(double)RAND_MAX;
 				if (rnd < pli){
 					//Generate position of particle IF to move left.
 					//Off-lattice if testx < 0, x[i] = 0 for that to occur.
@@ -125,7 +129,7 @@ int main(){
 			}
 			else{
 				//Apply extracellular conditions to test particle.
-				rnd = (float)rand()/(float)RAND_MAX;
+				rnd = (double)rand()/(double)RAND_MAX;
 				if (rnd < ple){
 					//Generate position of particle if to move left.
 					testx = x[i] - a;
@@ -162,7 +166,7 @@ int main(){
 				else if(state == 1 && teststate == 0){
 					//Particle is in intracellular region and at boundary.
 					//Draw rnd to determine movement.
-					rnd = (float)rand()/(float)RAND_MAX;
+					rnd = (double)rand()/(double)RAND_MAX;
 					if(rnd < pie)
 						//Particle will cross boundary into extracellular region.
 						x[i] = testx;
@@ -170,7 +174,7 @@ int main(){
 				else{
 					//Particle is in extracellular region and at boundary.
 					//Draw rnd to determine movement.
-					rnd = (float)rand()/(float)RAND_MAX;
+					rnd = (double)rand()/(double)RAND_MAX;
 					if(rnd < pei)
 						//Particle will cross boundary in intracellular region.
 						x[i] = testx;					
@@ -181,20 +185,19 @@ int main(){
 			sum_x += x[i];
 			sum_x2 += x[i]*x[i];
 
-			/*Shift the particle position and increment particle count at that
-			position rho[sxi] by 1 unit.*/
+			/*Convert particle position to integer and increment particle count 
+			at that	position rho[sxi] by 1 unit.*/
 			sxi = (int)(x[i]);
 			rho[sxi]++; 
 		}
 		
-		//Writing cell density data to file. First column is time.
-		//fprintf(outdists, "%d ",t);
+		//Writing density distribution data to file.
 		for(i = 0; i < xL; i++){fprintf(outdists, "%d ", rho[i]);}
 		fprintf(outdists,"\n");
 		
 		//Writing mean-square-displacement data to file.
-		avg_x = sum_x/(float)N;
-		avg_x2 = sum_x2/(float)N;
+		avg_x = sum_x/(double)N;
+		avg_x2 = sum_x2/(double)N;
 		fprintf(outstats, "%f %f %f\n", avg_x, avg_x2, avg_x2-avg_x*avg_x);
 	}
 	//return 0;
