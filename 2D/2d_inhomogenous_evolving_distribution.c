c7b908ad1dca4675d11049ac18355c68760cc8ee
Paul Ionele <p.ionele@gmail.com>
2016-02-18 15:20:32 -0500

    Finite diffference method for solving 2D diffusion problem on domain modelling simple cell.

---
+++ 2D/2d_inhomogenous_evolving_distribution.c
@@ -0,0 +1,239 @@
+/*
+Numerically solving the diffusion equation, on a lattice,
+boundary conditions applied.
+
+*/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+#define N 100	//can be set to whatever.
+#define xC 15
+#define xE 15
+#define nU 3
+
+int main(){
+	//Index variables; for-loops and time-step limit.
+	int i, j;
+	int t, tmax = 20000;
+
+	//Unit cell dimensions.
+	int xU = xC + xE;
+	int yU = yC + yE;
+	//Total lattice/array dimensions.
+	int xL = xU*nU;
+	int yL = yU*mU;
+
+	//Start lattice site (x,y); currently a cellular region.
+	int xSP = (int)(xL/2.0) - (int)(xC/2.0) - 1;
+	int ySP = (int)(yL/2.0) - (int)(yC/2.0) - 1;
+
+	//Unit cell of lattice and modified position of lattice.
+	int ucolx;
+	int ucoly;
+	int modx;
+	int mody;
+	//Used in converting position to integer.
+	int sxi;
+	int syi;
+
+	//Current time step and next time step density distribution arrays.
+	double rho_c[yL][xL];
+	double rho_n[yL][xL];
+
+	//Initializing density distribution arrays.
+	for (j = 0; j < yL; j++){
+			for(i = 0; i < xL; i++){
+				rho_c[j][i] = 0;
+				rho_n[j][i] = 0;
+			}
+		}
+
+	//Setting density at start position of particles.
+	rho_n[ySP][xSP] = N;
+
+	//Stepping probabilities (arb. chosen) for intracellular.
+	//Physical model: intracellular regions less diffusive (more viscous).
+	double pnxi = 0.2, ppxi = 0.2, psxi = 1.0 - pnxi - ppxi;
+	double pnyi = 0.2, ppyi = 0.2; psyi = 1.0 - pnyi - ppyi;
+	//Stepping probabilities (arb. chosen) for extracellular.
+	//Physical model: extracellular regions more diffusive (less viscous).
+	double pnxe = 0.4, ppxe = 0.4; psxe = 1.0 - pnxe - ppxe;
+	double pnye = 0.4, ppye = 0.4; psye = 1.0 - pnye - ppye;
+	//Transition probabilities between cell types.
+	double pie = 0.025;
+	double pei = (pnxi/pnxe)*pie;
+
+	//File naming and preparation. Why do we get a segmentation fault below?
+	//char *path = "/home/paul/Documents/thesis/particle-diffusion/data/";
+	//char *f1 = strcat(path,"TEST1.txt");
+	//char *f2 = strcat(path,"TEST1-stats.txt");
+	char *f1 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/t-3k_xU-3_pi-0.1_pe-0.2-pie-0.05.txt";
+	char *f2 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/t-3k_xU-3_pi-0.1_pe-0.2-pie-0.05_stats.txt";
+	FILE *outdists, *outstats;
+	outdists = fopen(f1, "w");
+	outstats = fopen(f2, "w");
+
+	for(t = 0; t < tmax; t++){
+		
+		//Update the current time step density distribution (dd).
+		//Now the current dd will be used to fill the next time-step array.
+		for (j = 0; j < yL; j++){
+			for(i = 0; i < xL; i++){
+				rho_c[j][i] = rho_n[j][i];
+				rho_n[j][i] = 0;
+			}
+		}
+
+		//Loop over all lattice sites and recalculate dds.
+		for(j = 0; j < yL; j++){
+			
+			for(i = 0; i < xL; i++){
+				
+				if(rho_c[j][i] == 0){
+					//No particles at current site, skip the remaining statements.
+					//Probably doesn't provide much speed improvement in long run?!
+					continue;
+				}
+
+				//Some clarifying statements to be added...
+				ucolx = (int)(i/xU);
+				ucoly = (int)(j/yU)
+				modx = (int)(i - ucolx*xU);
+				mody = (int)(j - ucoly*yU);
+
+				if( ((i != 0) & (i != (xL - 1))) & ((j != 0) & (j != (yL - 1)))){
+					//Not at absolute boundary.
+					
+					if(mody < yC){
+						/*
+						Lattice site is in top half of 2D array and may be in either an
+						intracelllar or extracellular region.
+						*/
+						if( modx < xC ){
+							//Lattice site is in cellular region.
+							if( ((modx != 0) & (modx != (xC-1))) & ((mody != 0) & (mody != (yC-1))) ){
+								//Lattice site is not at cellular boundaries.
+								rho_n[j][i] = 
+								(1.0-ppxi*psyi-pnxi*psyi-psxi*ppyi-psxi*pnyi-ppxi*ppyi-pnxi*pnyi-ppxi*pnyi-pnxi*ppyi)*rho_c[j][i] + 
+								ppxi*psyi*rho_c[j][i-1] +
+								pnxi*psyi*rho_c[j][i+1] +
+								psxi*ppyi*rho_c[j-1][i] +
+								psxi*pnyi*rho_c[j+1][i] +
+								ppxi*ppyi*rho_c[j-1][i-1] +
+								pnxi*pnyi*rho_c[j+1][i+1] +
+								ppxi*pnyi*rho_c[j+1][x-1] +
+								pnxi*ppyi*rho_c[j-1][x+1]
+							}
+							else if( mody == 0 & modx != 0 & modx != (xC-1) ){
+								//At the -y boundary, not at corner.
+								//Will this case ever happen?
+								printf("Error, should not happen with current setup.\n");
+							}
+							else if( mody == (yC-1) & modx != 0 & modx != (xC-1) ){
+								//At the +y boundary, not at corner.
+								rho_n[j][i] = 
+								(1.0-ppxi*psyi-pnxi*psyi-psxi*ppyi-psxe*pnye*pei-ppxi*ppyi-pnxe*pnye*pei-ppxe*pnye*pei-pnxi*ppyi)*rho_c[j][i] + 
+								ppxi*psyi*rho_c[j][i-1] +
+								pnxi*psyi*rho_c[j][i+1] +
+								psxi*ppyi*rho_c[j-1][i] +
+								psxe*pnye*pei*rho_c[j+1][i] +
+								ppxi*ppyi*rho_c[j-1][i-1] +
+								pnxe*pnye*pei*rho_c[j+1][i+1] +
+								ppxe*pnye*pei*rho_c[j+1][x-1] +
+								pnxi*ppyi*rho_c[j-1][x+1]
+							}
+							else if( modx == 0 & mody != 0 & mody != (yC-1) ){
+								//At the -x boundary, not at corner.
+								rho_n[j][i] = 
+								(1.0-ppxe*pei*psxe-pnxi*psxi-psxi*ppyi-psxi*pnyi-ppxe*pei*ppye-pnxi*pnyi-ppxe*pei*pnxe-pnxi*ppyi)*rho_c[j][i] + 
+								ppxe*pei*psxe*rho_c[j][i-1] +
+								pnxi*psxi*rho_c[j][i+1] +
+								psxi*ppyi*rho_c[j-1][i] +
+								psxi*pnyi*rpnxi*ppyiho_c[j+1][i] +
+								ppxe*pei*ppye*rho_c[j-1][i-1] +
+								pnxi*pnyi*rho_c[j+1][i+1] +
+								ppxe*pei*pnxe*rho_c[j+1][x-1] +
+								pnxi*ppyi*rho_c[j-1][x+1]
+							}
+							else if( modx == (xC-1) & mody != 0 & mody != (yC-1) ){
+								//At the +x boundary, not at a corner.
+								rho_n[j][i] = 
+								(1.0-ppxi*psyi-pnxe*pei*psye-psxi*ppyi-psxi*pnyi-ppxi*ppyi-pnxe*pei*pnye-ppxi*pnyi-pnxe*pei*ppye)*rho_c[j][i] + 
+								ppxi*psyi*rho_c[j][i-1] +
+								pnxe*pei*psye*rho_c[j][i+1] +
+								psxi*ppyi*rho_c[j-1][i] +
+								psxi*pnyi*rho_c[j+1][i] +
+								ppxi*ppyi*rho_c[j-1][i-1] +
+								pnxe*pei*pnye*rho_c[j+1][i+1] +
+								ppxi*pnyi*rho_c[j+1][x-1] +
+								pnxe*pei*ppye*rho_c[j-1][x+1]
+							}
+							else if( modx == 0 & mody == 0){
+								//At the -x,-y corner.
+								//Will this case ever happen?
+								printf("Error, should not happen.\n");
+							}
+							else if( modx == (xC-1) & mody == 0){
+								//At the +x,-y corner.
+								//Will this case ever happen?
+								printf("Error, should not happen.\n");
+							}
+							else if( modx == (xC-1) & mody == (yC-1) ){
+								//At the +x,+y corner.
+								rho_n[j][i] = 
+								(1.0-)*rho_c[j][i] + 
+								*rho_c[j][i-1] +
+								*rho_c[j][i+1] +
+								*rho_c[j-1][i] +
+								*rho_c[j+1][i] +
+								*rho_c[j-1][i-1] +
+								*rho_c[j+1][i+1] +
+								*rho_c[j+1][x-1] +
+								*rho_c[j-1][x+1]
+							}
+							else if( modx == 0 & mody == (yC-1) ){
+								//At the -x,+y corner.
+								rho_n[j][i] = 
+								(1.0-)*rho_c[j][i] + 
+								*rho_c[j][i-1] +
+								*rho_c[j][i+1] +
+								*rho_c[j-1][i] +
+								*rho_c[j+1][i] +
+								*rho_c[j-1][i-1] +
+								*rho_c[j+1][i+1] +
+								*rho_c[j+1][x-1] +
+								*rho_c[j-1][x+1]
+							}
+							else{
+								printf("Error in cellular region.\n")
+							}
+						}
+						else{
+							//Lattice site is in upper half extracellular region.
+
+							
+						}
+					}
+					else{
+						/*
+						The lattice site is in the lower half of the 2D array and is only
+						in extracellular region. Refer to cell model.
+						*/
+					}
+				}
+				else{
+					//At an absolute boundary.					
+				}
+			} //no touch
+		} //no touch
+
+
+		//Writing dd data to file.
+		for (i = 0; i < xL; i++){
+			fprintf(outdists, "%f ", rho_n[i]);
+		}
+		fprintf(outdists, "\n");
+	}
+}
