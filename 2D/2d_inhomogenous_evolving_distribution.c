ca2b6a4cd4d5d52fb0ac56fd14a9f24b7e155dcd
Paul Ionele <p.ionele@gmail.com>
2016-03-07 12:30:25 -0500

    MSDx not yet finished, minor updates.

--- 2D/2d_inhomogenous_evolving_distribution.c
+++ 2D/2d_inhomogenous_evolving_distribution.c
@@ -12,14 +12,14 @@ boundary conditions applied.
 #define xE 15
 #define yC 15
 #define yE 15
-#define nU 3
+#define nU 11
 #define mU 1
 
 int main(){
 	//Index variables; for-loops and time-step limit.
 	int i, j;
 	int mi, mj;
-	int t, tmax = 20000;
+	int t, tmax = 10000;
 
 	//Unit cell dimensions.
 	int xU = xC + xE;
@@ -46,7 +46,6 @@ int main(){
 	double rho_c[yL+1][xL+1];
 	double rho_n[yL+1][xL+1];
 	double mask[yL+1][xL+1];
-	
 	
 	//Probability distribution arrays.
 	double ppx[yL+1][xL+1];
@@ -80,9 +79,10 @@ int main(){
 	double pnxe = 0.2, ppxe = 0.2, psxe = 1.0 - pnxe - ppxe;
 	double pnye = 0.2, ppye = 0.2, psye = 1.0 - pnye - ppye;
 	//Transition probabilities between cell types.
-	double pie = 0.2;
+	double pie = 0.01;
 	double pei = (pnxi/pnxe)*pie;
 
+	//For analytics.
 	double sum_x, sum_x2, avg_x, avg_x2;
 	double sum_y, sum_y2, avg_y, avg_y2;
 	int count = 0;
@@ -91,145 +91,190 @@ int main(){
 	//char *path = "/home/paul/Documents/thesis/particle-diffusion/data/";
 	//char *f1 = strcat(path,"TEST1.txt");
 	//char *f2 = strcat(path,"TEST1-stats.txt");
-	char *f1 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/FD_test2.txt";
-	char *f2 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/FD_test2_stats.txt";
+	char *f1 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/FD_t-30k_N-1_nU-21_pi-0.05_pe-0.2_pie-0.01.txt";
+	char *f2 = "/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/FD_t-30k_N-1_nU-21_pi-0.05_pe-0.2_pie-0.01_stats.txt";
 	FILE *outdists, *outstats;
 	outdists = fopen(f1, "w+");
 	outstats = fopen(f2, "w+");
 
-	//Create mask of lattice points types
+	//Create mask of lattice point types.
 	for(j = 0; j <= yL; j++){
 		for(i = 0; i <= xL; i++){
-			mask[j][i]=2;
+
+			//Set entire system to type-2.
+			mask[j][i] = 2;
 			
 			ucolx = (int)(i/xU);
 			ucoly = (int)(j/yU);
 			modx = (int)(i - ucolx*xU);
 			mody = (int)(j - ucoly*yU);
 			
+			//Find and set those intracellular sites specifically to type-1.
 			if( (modx < xC) && (mody < yC) ){
-				mask[j][i]=1;	
+				mask[j][i] = 1;	
 			}
 		}
 	}
-	j=0;
+	
+	//Set the border sites to type-0. No lattice points used here.
+	//j = 0;
 	for(i = 0; i <= xL; i++){
-		mask[j][i]=0;
+		mask[0][i] = 0;
 	}
-	j=yL;
+	//j=yL; //why these?
 	for(i = 0; i <= xL; i++){
-		mask[j][i]=0;
+		mask[yL][i] = 0;
 	}	
-	i=0;
+	//i=0;
 	for(j = 0; j <= yL; j++){
-		mask[j][i]=0;
+		mask[j][0] = 0;
 	}
-	i=xL;
+	//i=xL;
 	for(j = 0; j <= yL; j++){
-		mask[j][i]=0;
+		mask[j][xL] = 0;
 	}
 
 	//Creating the probability matrix.
 	for(j = 0; j <= yL; j++){
 		for(i = 0; i <= xL; i++){
-			if(mask[j][i]!=0){
-				if(mask[j][i]==1){
-					//Inside a Cell
-					
-					mi=i-1;
-					mj=j;
-					if(mask[mj][mi]==0)
-						pnx[j][i]=0;
-					else if(mask[mj][mi]==1)
-						pnx[j][i]=pnxi;
-					else if(mask[mj][mi]==2)
-						pnx[j][i]=pnxi*pie;
-					else
-						printf("Error Mask!\n");
-					
-					mi=i+1;
-					mj=j;
-					if(mask[mj][mi]==0)
-						ppx[j][i]=0;
-					else if(mask[mj][mi]==1)
-						ppx[j][i]=ppxi;
-					else if(mask[mj][mi]==2)
-						ppx[j][i]=ppxi*pie;
-					else
-						printf("Error Mask!\n");
+			//Loop over all the lattice points.
 
-					mi=i;
-					mj=j-1;
-					if(mask[mj][mi]==0)
-						pny[j][i]=0;
-					else if(mask[mj][mi]==1)
-						pny[j][i]=pnyi;
-					else if(mask[mj][mi]==2)
-						pny[j][i]=pnyi*pie;
-					else
-						printf("Error Mask!\n");
-					
-					mi=i;
-					mj=j+1;
-					if(mask[mj][mi]==0)
-						ppy[j][i]=0;
-					else if(mask[mj][mi]==1)
-						ppy[j][i]=ppyi;
-					else if(mask[mj][mi]==2)
-						ppy[j][i]=ppyi*pie;
-					else
-						printf("Error Mask!\n");
+			if(mask[j][i] != 0){
+				//Lattice site is not a border lattice.
 
+				if(mask[j][i] == 1){
+					//Intracellular lattice point.
+					//mi and mj are modified indicies.
+
+					mi = i-1;
+					mj = j;
+					if(mask[mj][mi] == 0){
+						pnx[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						pnx[j][i] = pnxi;
+					}
+					else if(mask[mj][mi] == 2){
+						pnx[j][i] = pnxi*pie;
+					}
+					else{
+						printf("Error Mask at i-1!\n");
+					}
+					
+					mi = i+1;
+					mj = j;
+					if(mask[mj][mi] == 0){
+						ppx[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						ppx[j][i] = ppxi;
+					}
+					else if(mask[mj][mi] == 2){
+						ppx[j][i] = ppxi*pie;
+					}
+					else{
+						printf("Error Mask at i+1!\n");
+					}
+
+					mi = i;
+					mj = j-1;
+					if(mask[mj][mi] == 0){
+						pny[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						pny[j][i] = pnyi;
+					}
+					else if(mask[mj][mi] == 2){
+						pny[j][i] = pnyi*pie;
+					}
+					else{
+						printf("Error Mask at j-1!\n");
+					}
+					
+					mi = i;
+					mj = j+1;
+					if(mask[mj][mi] == 0){
+						ppy[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						ppy[j][i] = ppyi;
+					}
+					else if(mask[mj][mi] == 2){
+						ppy[j][i] = ppyi*pie;
+					}
+					else{
+						printf("Error Mask at j+1!\n");
+					}
+
+					//The probability of staying at current lattice site.
 					ps[j][i]=1.0-pnx[j][i]-ppx[j][i]-pny[j][i]-ppy[j][i];					
 
 				}
-				else if(mask[j][i]==2){
-					//Outside a Cell
+				else if(mask[j][i] == 2){
+					//Extracellular lattice points.
+					//mi and mj are modified indicies.
 					
-					mi=i-1;
-					mj=j;
-					if(mask[mj][mi]==0)
-						pnx[j][i]=0;
-					else if(mask[mj][mi]==1)
-						pnx[j][i]=pnxe*pei;
-					else if(mask[mj][mi]==2)
-						pnx[j][i]=pnxe;
-					else
-						printf("Error Mask!\n");
+					mi = i-1;
+					mj = j;
+					if(mask[mj][mi] == 0){
+						pnx[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						pnx[j][i] = pnxe*pei;
+					}
+					else if(mask[mj][mi] == 2){
+						pnx[j][i] = pnxe;
+					}
+					else{
+						printf("Error Mask at i-1!\n");
+					}
 					
-					mi=i+1;
-					mj=j;
-					if(mask[mj][mi]==0)
-						ppx[j][i]=0;
-					else if(mask[mj][mi]==1)
-						ppx[j][i]=ppxe*pei;
-					else if(mask[mj][mi]==2)
-						ppx[j][i]=ppxe;
-					else
-						printf("Error Mask!\n");
+					mi = i+1;
+					mj = j;
+					if(mask[mj][mi] == 0){
+						ppx[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						ppx[j][i] = ppxe*pei;
+					}
+					else if(mask[mj][mi] == 2){
+						ppx[j][i] = ppxe;
+					}
+					else{
+						printf("Error Mask at i+1!\n");
+					}
 
-					mi=i;
-					mj=j-1;
-					if(mask[mj][mi]==0)
-						pny[j][i]=0;
-					else if(mask[mj][mi]==1)
-						pny[j][i]=pnye*pei;
-					else if(mask[mj][mi]==2)
-						pny[j][i]=pnye;
-					else
-						printf("Error Mask!\n");
+					mi = i;
+					mj = j-1;
+					if(mask[mj][mi] == 0){
+						pny[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						pny[j][i] = pnye*pei;
+					}
+					else if(mask[mj][mi] == 2){
+						pny[j][i] = pnye;
+					}
+					else{
+						printf("Error Mask at j-1!\n");
+					}
 					
-					mi=i;
-					mj=j+1;
-					if(mask[mj][mi]==0)
-						ppy[j][i]=0;
-					else if(mask[mj][mi]==1)
-						ppy[j][i]=ppye*pei;
-					else if(mask[mj][mi]==2)
-						ppy[j][i]=ppye;
-					else
-						printf("Error Mask!\n");
+					mi = i;
+					mj = j+1;
+					if(mask[mj][mi] == 0){
+						ppy[j][i] = 0;
+					}
+					else if(mask[mj][mi] == 1){
+						ppy[j][i] = ppye*pei;
+					}
+					else if(mask[mj][mi] == 2){
+						ppy[j][i] = ppye;
+					}
+					else{
+						printf("Error Mask at j+1!\n");
+					}
 					
+					//The probability of staying at the current lattice site.
 					ps[j][i]=1.0-pnx[j][i]-ppx[j][i]-pny[j][i]-ppy[j][i];
 				}
 				else{
@@ -239,15 +284,19 @@ int main(){
 		}
 	}
 	
+	//The above is only calculated once.
 	for(t = 0; t < tmax; t++){
 		//Update the current time step density distribution (dd).
 		//Now the current dd will be used to fill the next time-step array.
+
+		//Resetting variables for analysis.
 		sum_x = 0;
 		sum_x2 = 0;
 		avg_x = 0;
 		avg_x2 = 0;
-		count = 0;
+		count = 0; //will equal the number of (real?) lattice site.
 
+		//Updating density distribution.
 		for (j = 0; j <= yL; j++){
 			for(i = 0; i <= xL; i++){
 				rho_c[j][i] = rho_n[j][i];
@@ -256,30 +305,36 @@ int main(){
 		}
 
 		//Loop over all lattice sites and recalculate dds.
-		float rhot=0;
+		float rhot = 0;
 		for(j = 0; j <= yL; j++){
 			for(i = 0; i <= xL; i++){
-				if(mask[j][i]!=0){
-					rho_n[j][i]=ps[j][i]*rho_c[j][i]
-						+ppx[j][i-1]*rho_c[j][i-1]
-						+pnx[j][i+1]*rho_c[j][i+1]
-						+ppy[j-1][i]*rho_c[j-1][i]
-						+pny[j+1][i]*rho_c[j+1][i];
+				if(mask[j][i] != 0){
+					rho_n[j][i] = 
+						ps[j][i]*rho_c[j][i] +
+						ppx[j][i-1]*rho_c[j][i-1] +
+						pnx[j][i+1]*rho_c[j][i+1] +
+						ppy[j-1][i]*rho_c[j-1][i] +
+						pny[j+1][i]*rho_c[j+1][i];
 				}
-				rhot+=rho_n[j][i];
+
+				//Total after sum should equal ~1.0 (if N = 1.0).
+				rhot += rho_n[j][i];
 				
 				// if(rho_n[j][i]>1.0){
 				// 	//printf("Arg!          %i %i %f\n",i,j,rho_n[j][i]);
 				// 	//getchar();
 				// }
 				sum_x += (float)i*rho_n[j][i];
-				sum_x2 += sum_x*sum_x;
+				sum_x2 += (float)i*rho_n[j][i] * (float)i*rho_n[j][i];
 				count += 1;
 
-			} //no touch
-		} //no touch
-		if(t%100==0)
+			}
+		}
+
+		//Print out a progress update.
+		if(t%100 == 0){
 			printf("%i %f\n",t,rhot);
+		}
 		
 		//Writing dd data to file.
 		for (j = 0; j <= yL; j++){
@@ -291,7 +346,7 @@ int main(){
 		}
 		fprintf(outdists, "\n");
 
-		//Writing MSD data to file.
+		//Writing MSD data to file, done for every time step.
 		avg_x = sum_x/(float)count;
 		avg_x2 = sum_x2/(float)count;
 		fprintf(outstats, "%f %f %f\n", avg_x, avg_x2, avg_x2-avg_x*avg_x);
