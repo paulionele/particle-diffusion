8154f6cb5b76a41043e1e801427b26ffbfd0e14a
Paul Ionele <p.ionele@gmail.com>
2016-03-13 23:20:15 -0400

    Changes ported from 2d_diffusion_msd script, for consistency. All working OK.

--- 1d_diffusion_msd.py
+++ 1d_diffusion_msd.py
@@ -18,59 +18,64 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import numpy as np
 
-def regression(times, msd_values):
+def regression(times, col3):
 	''' 
 	Function called for calculating linear regression.
 	Returns a set of parameters used for to construct a linear
 	fit plot to the computed data. Possible to 'window'.
 	'''
-	(slope, intercept, r_value, p_value, std_error) = stats.linregress(times, msd_values)
-	diffusion_coefficient  = slope/2 #redundent
-	return slope, intercept, diffusion_coefficient
+	(slope, intercept, r_value, p_value, std_error) = stats.linregress(times, col3)
+	#diffusion_coefficient  = slope/2 #redundent
+	return slope, intercept
 
 #scriptname, filename = argv #unpacking the argument variables
-filename = ''
-savepath = '/home/paul/Documents/thesis/particle-diffusion/2D/2D-data/'
+filename = 'IH000_stats.txt'
+savepath = '/home/paul/Documents/thesis/particle-diffusion/data/'
 filepath = path.join(savepath,filename)
 
 with open(filepath,'r') as f1:
-	msd_x = []
-	msd_y = []
-	msd_values = []
+	col1 = [] #typically mean x-position
+	col2 = [] #typically mean (x-position)^2
+	col3 = [] #typically MSD-x as calculated from the above quantities
 	for line in f1:
 		ls = line.split()
-		msd_x += [float(ls[0])]
-		msd_y += [float(ls[1])]
-		msd_values += [float(ls[0])]
-	ts = len(msd_values)
+		#Analytical data output is currently set at 3 cols.
+		#Make sure you are using the correct data. 
+		col1 += [float(ls[0])]
+		col2 += [float(ls[1])]
+		col3 += [float(ls[2])]
+	ts = len(col3)
 
 times = np.array(range(1,ts+1))
 
 #Regression time window. ts: time_start, th: time_halt
 ts1 = 0
-th1 = 5
+th1 = 10
 
 #Plotting time window.
 ts2 = 0
 th2 = ts
 
-#Fitting a linear curve through the data range specified in the regression window.
-(slope, B, D) = regression(times[ts1:th1], msd_values[ts1:th1])
-print('The effective diffusion D on [{},{}]: {}'.format(ts1, th1, D))
+###Fitting a line through the data range specified in the regression window.
+###Note the factor to obtain D from slope; 1/2 for 1D, 1/4 for 2D.
+(slope, B) = regression(times[ts1:th1], col3[ts1:th1])
+print('The effective diffusion D on [{},{}]: {}'.format(ts1, th1, slope/2))
 
-#Plotting the data.
-plt.plot(times[ts2:th2], msd_x[ts2:th2], color = 'blue', label = 'MSD_x')
-plt.plot(times[ts2:th2], msd_y[ts2:th2], color = 'green', label = 'MSD_y')
+###Plotting the data, MSD vs. time.
+plt.plot(times[ts2:th2], col3[ts2:th2], color = 'blue', label = 'MSD_x')
+#plt.plot(times[ts2:th2], msd_y[ts2:th2], color = 'green', label = 'MSD_y')
 #plt.plot(times[ts2:th2], msd_values[ts2:th2], color = 'red')
 
-#plt.plot(times[ts2:th2], 4*0.2*times[ts2:th2], '--r') #extracellular diffusion
-#plt.plot(times[ts2:th2], 4*0.05*times[ts2:th2], '--g') #crossing intra to extra
+###Plotting regression fit and theoretical curves.
+plt.plot(times[ts2:th2], slope*times[ts2:th2] + B, '--r', label = 'LinReg [{},{}]'.format(ts1, th1))
+plt.plot(times[ts2:th2], 2*0.1*times[ts2:th2], '--g', label = 'exact_i')
+
 
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('Time')
 plt.ylabel('MSD')
-plt.legend()
+plt.legend(loc=2)
 plt.show()
 
 #Plot of MSD/t vs. t
@@ -80,7 +85,7 @@ plt.show()
 #Can calculate a diffusion coefficient for every time step. Maybe plot that?
 
 #Writing probability and diffusion coefficient data to file...
-with open(savepath + 'TEST_diffusion_coeffs_v_probablities.txt','a') as f2:
+#with open(savepath + 'TEST_diffusion_coeffs_v_probablities.txt','a') as f2:
 	
 	#Searching for probability in file name. Probability of form 'xyz'.
 	#Comment out the code for inhomogenous case.
@@ -93,4 +98,4 @@ with open(savepath + 'TEST_diffusion_coeffs_v_probablities.txt','a') as f2:
 	# 	p += j
 	# probability = int(p[2:4])/100
 	# f2.write('{} {}\n'.format(probability, diffusion_coefficient))
-	f2.write('{}\n'.format(D))
+	#f2.write('{}\n'.format(D))
